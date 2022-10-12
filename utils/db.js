f43d03ce93ecfc44ba62f8607aa956b55cfe796f
Shahbaz-Ali <shahbazalidev@gmail.com>
2022-10-12 11:49:11 +0500

    added database connection file, & seed users to db

---
+++ utils/db.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+const connection = {};
+
+async function connect() {
+  // if connected then use previous and return
+  if (connection.isConnected) {
+    console.log('already connected');
+    return;
+  }
+  // checking if connections availavble
+  if (mongoose.connections.length > 0) {
+    // saving the connection state information to connection variable
+    connection.isConnected = mongoose.connections[0].readyState;
+    // if the connection is connected properly then return and use previous
+    if (connection.isConnected === 1) {
+      console.log('use previous');
+      return;
+    }
+    // if not connected properly then disconnect
+    await mongoose.disconnect();
+  }
+  // this is fired when the connection is not available
+  // here we are connecting to the database
+  const db = await mongoose.connect(process.env.MONGODB_URI);
+  // here we are storing the connection state to connection variable
+  connection.isConnected = db.connections[0].readyState;
+}
+
+async function disconnect() {
+  if (connection.isConnected) {
+    if (process.env.NODE_ENV === 'production') {
+      await mongoose.disconnect();
+    } else {
+      console.log('not disconnected');
+    }
+  }
+}
+
+const db = { connect, disconnect };
+export default db;
